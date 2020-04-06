31159d78fb8699926ff0b66c6dddb61828034154
Juani Unzurrunzaga <juanunzu@gmail.com>
2020-04-06 20:17:52 -0300

    layout mostrar terminado

---
+++ app/src/main/java/com/example/comunicacionactivities/layout2Activity.java
@@ -0,0 +1,14 @@
+package com.example.comunicacionactivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class layout2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout2);
+    }
+}
